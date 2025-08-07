7736b2910ef9c01f453d14f4a220d3f359a1cc55
saltyfish <zou2745731645@qq.com>
2025-08-07 21:01:22 +0800

    (feat): /backend/readFile API. reading dynamic file

--- backend/src/main/java/com/saltyfish/backend/config/WebMvcConfiguration.java
+++ backend/src/main/java/com/saltyfish/backend/config/WebMvcConfiguration.java
@@ -41,7 +41,8 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/login", "/register", "/applyForEmailcode", "/isAccountExist", "/retrieve");
+                .excludePathPatterns("/login", "/register", "/applyForEmailcode", "/isAccountExist", "/retrieve",
+                        "/backend/**");
     }
 
     @Override
